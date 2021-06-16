ab8fdc8c6b845fe0bb3cd247853f5b5846c615bd
Tijani Fulani <Fulani07@gmail.com>
2021-06-16 14:30:08 +0100

    managed to get the fetch and crypto list

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import MainDetail from "./components/MainDetail";
+import { CRYPTO_LIST } from "./constants";
 
 function App() {
   // This piece of state keeps the id from the selected coin to be displayed in the MainDetail component
   const [selectedCrypto, setSelectedCrypto] = useState(null);
-
+  const [cryptoList, setCryptoList] = useState([])
   // This function gives you whether a coin has been selected or not
   // You will need this for the SideListItem component
   function isSelectedCrypto(id) {
     return selectedCrypto === id;
   }
 
+  useEffect(() => {
+    return fetch(CRYPTO_LIST).then(resp => resp.json()).then(listOfCrypto => setCryptoList(listOfCrypto))
+  }, [])
+  
   return (
     /* These (<> </>) are called React Fragments, and allow us to return more than one top element */
     <>
--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const API = 'https://api.coingecko.com/api/v3';
 
 export const STATUS_UPDATES = `${API}/status_updates`;
-export const CRyPTO_LIST = `${API}/coins/markets?vs_currency=gbp`;
+export const CRYPTO_LIST = `${API}/coins/markets?vs_currency=gbp`;
 export const getCryptoUpdateUrl = (id) =>
   `${API}/simple/price?ids=${id}&vs_currencies=gbp&include_last_updated_at=true`;
