907db58837a5752602b39832ece154a225a37340
Tijani Fulani <Fulani07@gmail.com>
2021-06-16 19:50:51 +0100

    .

--- src/components/MainDetail.jsx
+++ src/components/MainDetail.jsx
@@ -20,9 +20,11 @@ function convertToSeconds(dateValue) {
 
 export default function MainDetail({foundCrypto}) {
 
+  const {name, symbol, last_updated, current_price} = foundCrypto
+
   const [currentTime, setCurrentTime] = useState(getCurrentTime())
 
-  const dateValue = foundCrypto.last_updated
+  const dateValue = last_updated
 
   const seconds =  convertToSeconds(dateValue)
   const timeToDisplay = currentTime - seconds
@@ -44,11 +46,11 @@ export default function MainDetail({foundCrypto}) {
   <div className="main-detail__update">
   </div>
   <div className="main-detail__name">
-    <h2>{foundCrypto.name}</h2>
-    <p><span className="small">a.k.a </span>{foundCrypto.symbol}</p>
+    <h2>{name}</h2>
+    <p><span className="small">a.k.a </span>{symbol}</p>
   </div>
   <div className="main-detail__price">
-    <p>{`£ ${foundCrypto.current_price.toFixed(2)}`}</p>
+    <p>{`£${current_price.toFixed(2)}`}</p>
     <p>{`Updated ${timeToDisplay} seconds ago`}</p>
   </div>
 </section>
